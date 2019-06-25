f0f2f327db91ab217c4b62c11ab8bccf27be3eb9
mt26691 <tung.nguyen@scopicsoftware.com>
2019-06-25 20:45:18 +0700

    Check string is unique

---
+++ InterviewKit/Chapter01.01.IsUnique/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter01._01.IsUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Check all char in string is unique
+        /// </summary>
+        /// <param name="checkString"></param>
+        /// <returns></returns>
+        public static bool IsUnique(string checkString)
+        {
+            var dic = new Dictionary<char, bool>();
+            for (var i = 0; i < checkString.Length; i++)
+            {
+                if (dic.ContainsKey(checkString[i]))
+                {
+                    return false;
+                }
+                dic.Add(checkString[i], true);
+            }
+            return true;
+        }
+    }
+}
---
+++ InterviewKit/DictionaryHashMap.03.SherlockAndAnagrams/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DictionaryHashMap._03.SherlockAndAnagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        int sherlockAndAnagrams(string s)
+        {
+
+
+        }
+    }
+}
