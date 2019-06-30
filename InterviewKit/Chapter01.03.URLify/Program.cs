9c235ecc0c7e656a4fdcb403794d9a3acf586341
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-06-30 14:52:31 +0700

    05

---
+++ InterviewKit/Chapter01.03.URLify/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chapter01._03.URLify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UrlifyTest("test 01 02"));
+            Console.ReadLine();
+        }
+
+        public static string UrlifyTest(string inputUrl)
+        {
+            var result = inputUrl.Trim().Replace(" ", "%20");
+            return result;
+        }
+    }
+}
---
+++ InterviewKit/Chapter01.04.PalindromePermutation/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter01._04.PalindromePermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindromePermutation("aaaabbbccc"));
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Palindrome Permutation string can't have more than 1 odd char
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public static bool IsPalindromePermutation(string inputString)
+        {
+            var frequencyDic = BuildFrequencyTable(inputString);
+
+            int oddCount = 0;
+            foreach (var item in frequencyDic)
+            {
+                if (item.Value % 2 == 1)
+                {
+                    oddCount++;
+                    if (oddCount > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+
+            return true;
+        }
+        public static Dictionary<char, int> BuildFrequencyTable(string inputString)
+        {
+            var result = new Dictionary<char, int>();
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                var currentChar = inputString[i];
+                if (result.ContainsKey(currentChar))
+                {
+                    result[currentChar] += 1;
+                }
+                else
+                {
+                    result.Add(currentChar, 1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
