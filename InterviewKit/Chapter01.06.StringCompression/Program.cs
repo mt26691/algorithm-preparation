33cf834715e18c9e5d74e990bdf784fdc789a934
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-07-01 21:10:10 +0700

    compress string

---
+++ InterviewKit/Chapter01.06.StringCompression/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Chapter01._06.StringCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CompressString("aaabbcccddde"));
+            Console.ReadLine();
+        }
+
+        public static string CompressString(string inputString)
+        {
+            var builder = new StringBuilder();
+            var count = 0;
+            for (var i = 0; i < inputString.Length; i++)
+            {
+                count++;
+
+                if (i + 1 >= inputString.Length || inputString[i + 1] != inputString[i])
+                {
+                    builder.Append(inputString[i]);
+                    builder.Append(count);
+                    count = 0;
+                }
+            }
+            return builder.ToString();
+        }
+    }
+}
