b5c873e51739da5eee9c61d61e939a7cb8aa354d
mt26691 <tung.nguyen@scopicsoftware.com>
2019-07-03 10:30:28 +0700

    delete kth node

---
+++ InterviewKit/Chapter02.02.KthToLastElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter02._02.KthToLastElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddLast(i);
+            }
+
+            PrintKthToLast(list.First, 4);
+        }
+
+        public static void PrintKthToLast(LinkedListNode<int> head, int k)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                if (head.Next == null)
+                {
+                    return;
+                }
+                head = head.Next;
+            }
+
+            while(head != null)
+            {
+                Console.WriteLine(head.Value);
+                head = head.Next;
+            }
+        }
+
+    }
+}
---
+++ InterviewKit/Chapter02.03.DeleteMiddleNode/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter02._03.DeleteMiddleNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList();
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddLast(i);
+            }
+
+            var middleNode = GetMiddleNode(list.First, 4);
+            Console.WriteLine(middleNode.Value);
+            list.PrintNode();
+            list.DeleteNode(middleNode);
+            list.PrintNode();
+            Console.ReadLine();
+        }
+
+        public static LinkedListNode GetMiddleNode(LinkedListNode head, int k)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                if (head.Next == null)
+                {
+                    return null;
+                }
+                head = head.Next;
+            }
+
+            return head;
+        }
+
+        public static void DeleteMiddleNode(LinkedListNode<int> middleNode)
+        {
+            // point previous to next
+            if (middleNode == null)
+            {
+                return;
+            }
+        }
+    }
+
+    public class LinkedList
+    {
+
+        public LinkedListNode First { get; set; }
+
+        public LinkedListNode Last { get; set; }
+
+        public int Count { get; private set; } = 0;
+        public LinkedList()
+        {
+            First = null;
+            Last = null;
+        }
+
+        public LinkedListNode AddFirst(int value)
+        {
+            var node = new LinkedListNode(value);
+            if (First == null)
+            {
+                First = node;
+                if (Last == null)
+                {
+                    Last = node;
+                }
+            }
+
+            if (First != null)
+            {
+                First.Previous = node;
+                node.Next = First;
+                First = node;
+            }
+            Count++;
+            return node;
+        }
+
+        public LinkedListNode AddLast(int value)
+        {
+            var node = new LinkedListNode(value);
+            if (Last == null)
+            {
+                Last = node;
+                if (First == null)
+                {
+                    First = node;
+                }
+            }
+
+            if (Last != null)
+            {
+                Last.Next = node;
+                node.Previous = Last;
+                Last = node;
+            }
+            Count++;
+            return node;
+        }
+
+        public void DeleteNode(LinkedListNode node)
+        {
+            if(node == null)
+            {
+                return;
+            }
+            // if this is first node
+            if(node.Previous == null)
+            {
+                First = node.Next;
+            }
+            
+            // if this is last node
+            if(node.Next == null)
+            {
+                Last = node.Previous;
+            }
+           
+            if(node.Previous != null && node.Next != null)
+            {
+                node.Previous.Next = node.Next;
+                node.Next.Previous = node.Previous;
+            }
+
+            Count--;
+        }
+
+        public void PrintNode()
+        {
+            var head = First;
+            while (head != null)
+            {
+                Console.Write(head.Value + ", ");
+                head = head.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class LinkedListNode
+    {
+        public int Value { get; set; }
+
+        public LinkedListNode Next { get; set; }
+
+        public LinkedListNode Previous { get; set; }
+
+        public LinkedListNode(int value)
+        {
+            Value = value;
+        }
+    }
+}
