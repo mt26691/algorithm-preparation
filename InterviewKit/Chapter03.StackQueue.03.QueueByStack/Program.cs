33171aa43ce77bf46e3f3e2f13731793b3074dd3
mt26691 <tung.nguyen@scopicsoftware.com>
2019-07-06 11:04:59 +0700

    queue by 2 stacks

---
+++ InterviewKit/Chapter03.StackQueue.03.QueueByStack/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter03.StackQueue._03.QueueByStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<int> test = new MyQueue<int>();
+            test.EnQueue(1);
+            test.EnQueue(3);
+            test.EnQueue(2);
+            Console.WriteLine(test.Dequeue());
+            Console.WriteLine(test.Dequeue());
+            Console.WriteLine(test.Dequeue());
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+    public class MyQueue<T> where T : IComparable<T>
+    {
+        Stack<T> newestStack = new Stack<T>();
+        Stack<T> latestStack = new Stack<T>();
+
+        private void ShiftStack()
+        {
+            if (latestStack.Count == 0)
+            {
+                while (newestStack.Count > 0)
+                {
+                    latestStack.Push(newestStack.Pop());
+                }
+            }
+        }
+        public void EnQueue(T value)
+        {
+            newestStack.Push(value);
+        }
+
+        public T Dequeue()
+        {
+            ShiftStack();
+            return latestStack.Pop();
+        }
+
+        public int Count()
+        {
+            return newestStack.Count + latestStack.Count;
+        }
+    }
+}
---
+++ InterviewKit/Chapter03.StackQueue.03.StackOfPlates/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter03.StackQueue._03.StackOfPlates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class SetOfStack<T> where T : IComparable<T>
+    {
+        private List<Stack<T>> stacks = new List<Stack<T>>();
+
+        private Stack<T> lastStack;
+        private Stack<T> firstStack;
+
+        private int defaultCapacity = 5;
+        public SetOfStack()
+        {
+            lastStack = new Stack<T>();
+            firstStack = lastStack;
+            stacks = new List<Stack<T>>();
+            stacks.Add(lastStack);
+        }
+
+        public void Push(T value)
+        {
+            if(IsFullLastStack())
+            {
+                lastStack = new Stack<T>();
+                lastStack.Push(value);
+                stacks.Add(lastStack);
+                return;
+            }
+
+            lastStack.Push(value);
+        }
+
+        public T Pop()
+        {
+            if(lastStack.Count == 0)
+            {
+                stacks.Remove(lastStack);
+                lastStack = stacks.Last();
+            }
+
+            var item = lastStack.Pop();
+
+            return item;
+        }
+
+        public T PopAt(int index)
+        {
+            var currentStack = stacks[index];
+            return currentStack.Pop();
+        }
+
+        private bool IsFullLastStack()
+        {
+            return lastStack.Count == defaultCapacity;
+        }
+    }
+}
