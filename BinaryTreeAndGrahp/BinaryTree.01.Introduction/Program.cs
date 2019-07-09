c01e08d26969b9c05df65492e5288d788cf95792
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-07-09 20:32:37 +0700

    create binary tree node

---
+++ BinaryTreeAndGrahp/BinaryTree.01.Introduction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTree._01.Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
