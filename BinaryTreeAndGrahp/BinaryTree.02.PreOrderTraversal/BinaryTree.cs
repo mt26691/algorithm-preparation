c22481dd5de0492af74c4effb3ef0e9ac5237442
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-07-10 09:50:30 +0700

    binaytree traversal

--- BinaryTreeAndGrahp/BinaryTree.02.PreOrderTraversal/BinaryTree.cs
+++ BinaryTreeAndGrahp/BinaryTree.02.PreOrderTraversal/BinaryTree.cs
@@ -11,13 +11,33 @@ namespace BinaryTree._02.PreOrderTraversal
             Root = new BinaryTreeNode<T>(rootValue);
         }
 
-        public void InorderTraverse(BinaryTreeNode<T> node)
+        public void PreorderTraverse(BinaryTreeNode<T> node)
         {
             if (node != null)
             {
                 Console.WriteLine(node.Value);
+                PreorderTraverse(node.Left);
+                PreorderTraverse(node.Right);
+            }
+        }
+
+        public void InorderTraverse(BinaryTreeNode<T> node)
+        {
+            if (node != null)
+            {
                 InorderTraverse(node.Left);
+                Console.WriteLine(node.Value);
                 InorderTraverse(node.Right);
+            }
+        }
+
+        public void PostOrderTraverse(BinaryTreeNode<T> node)
+        {
+            if (node != null)
+            {
+                PostOrderTraverse(node.Left);
+                PostOrderTraverse(node.Right);
+                Console.WriteLine(node.Value);
             }
         }
     }
--- BinaryTreeAndGrahp/BinaryTree.02.PreOrderTraversal/Program.cs
+++ BinaryTreeAndGrahp/BinaryTree.02.PreOrderTraversal/Program.cs
@@ -9,10 +9,24 @@ namespace BinaryTree._02.PreOrderTraversal
             var binayTree = new BinaryTree<int>(5);
 
             binayTree.Root.Left = new BinaryTreeNode<int>(4);
-            binayTree.Root.Right = new BinaryTreeNode<int>(3);
             binayTree.Root.Left.Left = new BinaryTreeNode<int>(2);
             binayTree.Root.Left.Right = new BinaryTreeNode<int>(1);
+
+            binayTree.Root.Right = new BinaryTreeNode<int>(3);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(9);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(8);
+
+            Console.WriteLine("PreorderTraverse");
+            binayTree.PreorderTraverse(binayTree.Root);
+            Console.WriteLine("End PreorderTraverse");
+
+            Console.WriteLine("InorderTraverse");
             binayTree.InorderTraverse(binayTree.Root);
+            Console.WriteLine("End InorderTraverse");
+
+            Console.WriteLine("PostOrderTraverse");
+            binayTree.PostOrderTraverse(binayTree.Root);
+            Console.WriteLine("End PostOrderTraverse");
             Console.ReadLine();
         }
     }
