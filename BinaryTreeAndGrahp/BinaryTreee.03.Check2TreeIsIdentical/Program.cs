c4e63a5287f16a18caac2bb433efcccee1855222
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-07-10 09:59:46 +0700

    Is Identical binary tree

--- BinaryTreeAndGrahp/BinaryTreee.03.Check2TreeIsIdentical/Program.cs
+++ BinaryTreeAndGrahp/BinaryTreee.03.Check2TreeIsIdentical/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BinaryTree;
+using System;
 
 namespace BinaryTreee._03.Check2TreeIsIdentical
 {
@@ -6,7 +7,38 @@ namespace BinaryTreee._03.Check2TreeIsIdentical
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var binayTree = new BinaryTree<int>(5);
+
+            binayTree.Root.Left = new BinaryTreeNode<int>(4);
+            binayTree.Root.Left.Left = new BinaryTreeNode<int>(2);
+            binayTree.Root.Left.Right = new BinaryTreeNode<int>(1);
+
+            binayTree.Root.Right = new BinaryTreeNode<int>(3);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(9);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(8);
+
+            var binayTree1 = new BinaryTree<int>(5);
+            binayTree1.Root.Left = new BinaryTreeNode<int>(4);
+            binayTree1.Root.Left.Left = new BinaryTreeNode<int>(2);
+            binayTree1.Root.Left.Right = new BinaryTreeNode<int>(1);
+
+            binayTree1.Root.Right = new BinaryTreeNode<int>(3);
+            binayTree1.Root.Right.Left = new BinaryTreeNode<int>(9);
+            binayTree1.Root.Right.Right = new BinaryTreeNode<int>(8);
+
+            Console.WriteLine(IsIdentical(binayTree.Root, binayTree1.Root));
+            Console.ReadLine();
+        }
+
+        public static bool IsIdentical(BinaryTreeNode<int> node1, BinaryTreeNode<int> node2)
+        {
+            if(node1 == null && node2 == null)
+            {
+                return true;
+            }
+
+            return (node1 != null && node2 != null && node1.Value == node2.Value)
+                && IsIdentical(node1.Left, node2.Left) && IsIdentical(node1.Right, node2.Right);
         }
     }
 }
