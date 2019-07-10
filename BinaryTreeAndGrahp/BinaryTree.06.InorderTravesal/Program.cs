73a6eafe4825105f18ae55d17a49646bdbb003f0
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-07-10 10:35:32 +0700

    inorder travesal

---
+++ BinaryTreeAndGrahp/BinaryTree.06.InorderTravesal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BinaryTree._06.InorderTravesal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binayTree = new BinaryTree<int>(5);
+
+            binayTree.Root.Left = new BinaryTreeNode<int>(4);
+            binayTree.Root.Left.Left = new BinaryTreeNode<int>(2);
+            binayTree.Root.Left.Right = new BinaryTreeNode<int>(1);
+
+            binayTree.Root.Right = new BinaryTreeNode<int>(3);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(9);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(8);
+
+            binayTree.Root.Right.Right.Left = new BinaryTreeNode<int>(7);
+            binayTree.Root.Right.Right.Right = new BinaryTreeNode<int>(6);
+            InOrderTraverse(binayTree.Root);
+            Console.ReadLine();
+        }
+
+        public static void InOrderTraverse<T>(BinaryTreeNode<T> rootNode) where T : IComparable<T>
+        {
+            if(rootNode == null)
+            {
+                return;
+            }
+            InOrderTraverse<T>(rootNode.Left);
+            Console.WriteLine($"Travel to {rootNode.Value}");
+            InOrderTraverse<T>(rootNode.Right);
+        }
+    }
+}
