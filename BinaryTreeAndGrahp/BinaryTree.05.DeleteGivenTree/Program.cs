8285cf1de85d7d7a3648ed2099c425e34ce01123
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-07-10 10:26:11 +0700

    delete node

---
+++ BinaryTreeAndGrahp/BinaryTree.05.DeleteGivenTree/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BinaryTree._05.DeleteGivenTree
+{
+    /// <summary>
+    /// https://www.techiedelight.com/delete-given-binary-tree-iterative-recursive/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binayTree = new BinaryTree<int>(5);
+
+            binayTree.Root.Left = new BinaryTreeNode<int>(4);
+            binayTree.Root.Left.Left = new BinaryTreeNode<int>(2);
+            binayTree.Root.Left.Right = new BinaryTreeNode<int>(1);
+
+            binayTree.Root.Right = new BinaryTreeNode<int>(3);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(9);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(8);
+
+            binayTree.Root.Right.Right.Left = new BinaryTreeNode<int>(8);
+            DeleteTree(binayTree.Root);
+            Console.ReadLine();
+        }
+        
+        public static void DeleteTree(BinaryTreeNode<int> rootNode)
+        {
+            if(rootNode == null)
+            {
+                return;
+            }
+
+            DeleteTree(rootNode.Left);
+            DeleteTree(rootNode.Right);
+            Console.WriteLine($"Delete node with value = {rootNode.Value}");
+            rootNode = null;
+        }
+    }
+}
