d562dc6cf85dd95d70a3320af32f40416baa54fc
mt26691 <tung.nguyen@scopicsoftware.com>
2019-07-11 10:49:41 +0700

    spirial order tree

--- BinaryTreeAndGrahp/BinaryTree/BinaryTree.cs
+++ BinaryTreeAndGrahp/BinaryTree/BinaryTree.cs
@@ -45,7 +45,7 @@ namespace BinaryTree
         public void LevelOrderTraversal()
         {
             var root = Root;
-            if(root == null)
+            if (root == null)
             {
                 return;
             }
@@ -53,12 +53,12 @@ namespace BinaryTree
             var queue = new Queue<BinaryTreeNode<T>>();
             queue.Enqueue(Root);
 
-            while(queue.Count>0)
+            while (queue.Count > 0)
             {
                 var currentNode = queue.Dequeue();
                 Console.WriteLine($"Travel to {currentNode.Value}");
 
-                if(currentNode.Left != null)
+                if (currentNode.Left != null)
                 {
                     queue.Enqueue(currentNode.Left);
                 }
@@ -68,6 +68,73 @@ namespace BinaryTree
                     queue.Enqueue(currentNode.Right);
                 }
 
+            }
+        }
+
+        public void SpiralOrderTraversal()
+        {
+            var root = Root;
+            if (root == null)
+            {
+                return;
+            }
+
+            var queue = new Queue<BinaryTreeNode<T>>();
+            var stack = new Stack<BinaryTreeNode<T>>();
+            var tempQueue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(Root);
+
+            var fromLeftToRight = true;
+
+            while (true)
+            {
+                var queueCount = queue.Count;
+                var stackCount = stack.Count;
+                if (queueCount == 0 && stackCount == 0)
+                {
+                    return;
+                }
+
+                if (fromLeftToRight)
+                {
+                    while (queueCount > 0)
+                    {
+                        var data = queue.Dequeue();
+                        Console.WriteLine($"Travel to {data.Value}");
+                        if (data.Left != null) 
+                        {
+                            stack.Push(data.Left);
+                            tempQueue.Enqueue(data.Left);
+                        }
+                        if (data.Right != null)
+                        {
+                            stack.Push(data.Right);
+                            tempQueue.Enqueue(data.Right);
+                        }
+                        queueCount = queue.Count;
+                    }
+                }
+                else
+                {
+                    while (stackCount > 0)
+                    {
+                        var data = stack.Pop();
+                        Console.WriteLine($"Travel to {data.Value}");
+                        var queueData = tempQueue.Dequeue();
+
+                        if (queueData.Left != null) 
+                        {
+                            queue.Enqueue(queueData.Left);
+                        }
+                        if (queueData.Right != null)
+                        {
+                            queue.Enqueue(queueData.Right);
+                        }
+                        stackCount = stack.Count;
+                    }
+                }
+
+                fromLeftToRight = !fromLeftToRight;
             }
         }
     }
