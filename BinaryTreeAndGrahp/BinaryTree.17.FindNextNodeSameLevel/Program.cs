641e2ab2828dfaeb89919cd56bf494dfd0b457fe
mt26691 <tung.nguyen@scopicsoftware.com>
2019-07-13 16:31:12 +0700

    check incomplete tree

---
+++ BinaryTreeAndGrahp/BinaryTree.17.FindNextNodeSameLevel/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree._17.FindNextNodeSameLevel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binayTree = new BinaryTree<int>(1);
+
+            // level 2
+            binayTree.Root.Left = new BinaryTreeNode<int>(2);
+            binayTree.Root.Right = new BinaryTreeNode<int>(3);
+
+            // level 3
+            binayTree.Root.Left.Left = new BinaryTreeNode<int>(4);
+            binayTree.Root.Left.Right = new BinaryTreeNode<int>(5);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(6);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(7);
+            // level 4
+            binayTree.Root.Right.Left.Left = new BinaryTreeNode<int>(8);
+            binayTree.Root.Right.Left.Right = new BinaryTreeNode<int>(9);
+
+            var nextNode = FindNextNode(binayTree.Root, binayTree.Root.Right.Right);
+
+            if(nextNode == null)
+            {
+                Console.WriteLine("null");
+            }
+            else
+            {
+                Console.WriteLine(nextNode.Value);
+            }
+
+            Console.ReadLine();
+        }
+
+        
+
+        public static BinaryTreeNode<T> FindNextNode<T>(BinaryTreeNode<T> rootNode, BinaryTreeNode<T> givenNode) where T : IComparable<T>
+        {
+            if (rootNode == null || givenNode == null)
+            {
+                return null;
+            }
+
+            var queue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(rootNode);
+
+            int level = 0;
+
+            while (queue.Count > 0)
+            {
+                level++;
+                var queueCount = queue.Count;
+                while (queueCount-- > 0)
+                {
+                    var nodePool = queue.Dequeue();
+                    if(nodePool == givenNode)
+                    {
+                        // find next node immediately
+                        // if queue count == 0;
+                        if(queueCount == 0)
+                        {
+                            return null;
+                        }
+                        var nextNode = queue.Dequeue();
+                        return nextNode;
+                    }
+
+                    if(nodePool.Left != null)
+                    {
+                        queue.Enqueue(nodePool.Left);
+                    }
+
+                    if (nodePool.Right != null)
+                    {
+                        queue.Enqueue(nodePool.Right);
+                    }
+                }
+            }
+
+            Console.WriteLine("Node not found");
+            return null;
+
+        }
+
+    }
+}
---
+++ BinaryTreeAndGrahp/BinaryTree.18.CheckCompleteTreeOrNot/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree._18.CheckCompleteTreeOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binayTree = new BinaryTree<int>(1);
+
+            // level 2
+            binayTree.Root.Left = new BinaryTreeNode<int>(2);
+            binayTree.Root.Right = new BinaryTreeNode<int>(3);
+
+            // level 3
+            binayTree.Root.Left.Left = new BinaryTreeNode<int>(4);
+            binayTree.Root.Left.Right = new BinaryTreeNode<int>(5);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(6);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(7);
+            // level 4
+            binayTree.Root.Right.Left.Left = new BinaryTreeNode<int>(8);
+            binayTree.Root.Right.Left.Right = new BinaryTreeNode<int>(9);
+
+            var completeTree = new BinaryTree<int>(1);
+
+            // level 2
+            completeTree.Root.Left = new BinaryTreeNode<int>(2);
+            completeTree.Root.Right = new BinaryTreeNode<int>(3);
+
+            // level 3
+            completeTree.Root.Left.Left = new BinaryTreeNode<int>(4);
+            completeTree.Root.Left.Right = new BinaryTreeNode<int>(5);
+            completeTree.Root.Right.Left = new BinaryTreeNode<int>(6);
+            completeTree.Root.Right.Right = new BinaryTreeNode<int>(7);
+
+            var inCompleteTree = new BinaryTree<int>(1);
+
+            // level 2
+            inCompleteTree.Root.Left = new BinaryTreeNode<int>(2);
+            inCompleteTree.Root.Right = new BinaryTreeNode<int>(3);
+
+            // level 3
+            inCompleteTree.Root.Left.Left = new BinaryTreeNode<int>(4);
+            inCompleteTree.Root.Left.Right = new BinaryTreeNode<int>(5);
+            inCompleteTree.Root.Right.Right = new BinaryTreeNode<int>(7);
+
+
+            CheckCompleteTree(binayTree.Root);
+            CheckCompleteTree(completeTree.Root);
+            CheckCompleteTree(inCompleteTree.Root);
+            Console.ReadLine();
+        }
+
+
+
+        public static void CheckCompleteTree<T>(BinaryTreeNode<T> rootNode) where T : IComparable<T>
+        {
+            if (rootNode == null )
+            {
+                return;
+            }
+
+            var queue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(rootNode);
+
+            int level = 0;
+
+            while (queue.Count > 0)
+            {
+                level++;
+                var queueCount = queue.Count;
+                var previousIsNull = false;
+                while (queueCount-- > 0)
+                {
+                    var nodePool = queue.Dequeue();
+                    
+                    if(nodePool.Left == null && nodePool.Right != null)
+                    {
+                        Console.WriteLine("This is not the complete tree");
+                        return;
+                    }
+
+                    if(nodePool.Left == null || nodePool.Right == null)
+                    {
+                        previousIsNull = true;
+                    }
+
+                    if(previousIsNull && (nodePool.Left != null || nodePool.Right != null))
+                    {
+                        Console.WriteLine("This is not the complete tree");
+                        return;
+                    }
+                    if (nodePool.Left != null)
+                    {
+                        queue.Enqueue(nodePool.Left);
+                    }
+
+                    if (nodePool.Right != null)
+                    {
+                        queue.Enqueue(nodePool.Right);
+                    }
+                }
+            }
+
+            Console.WriteLine("This is the complete tree");
+
+        }
+
+    }
+}
