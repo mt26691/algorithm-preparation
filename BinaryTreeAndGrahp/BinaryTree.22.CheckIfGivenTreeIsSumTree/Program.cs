81f9137dee8eeb94034718ac45b3d15010713151
mt26691 <tung.nguyen@scopicsoftware.com>
2019-07-16 09:56:23 +0700

    check if they are the sub tree

---
+++ BinaryTreeAndGrahp/BinaryTree.22.CheckIfGivenTreeIsSumTree/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTree._22.CheckIfGivenTreeIsSumTree
+{
+
+    /// <summary>
+    /// https://www.techiedelight.com/check-given-binary-tree-sum-tree-not/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binayTree = new BinaryTree<int>(1);
+
+            // level 2
+            binayTree.Root.Left = new BinaryTreeNode<int>(2);
+            binayTree.Root.Right = new BinaryTreeNode<int>(3);
+
+            // level 3
+            binayTree.Root.Left.Left = new BinaryTreeNode<int>(4);
+            binayTree.Root.Left.Right = new BinaryTreeNode<int>(5);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(6);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(7);
+            // level 4
+            binayTree.Root.Right.Left.Left = new BinaryTreeNode<int>(8);
+            binayTree.Root.Right.Left.Right = new BinaryTreeNode<int>(9);
+
+            IsSumTree(binayTree.Root);
+            CalculateSumTree(binayTree.Root);
+            IsSumTree(binayTree.Root);
+            binayTree.LevelOrderTraversal();
+
+            Console.ReadLine();
+        }
+
+        public static bool IsSumTree(BinaryTreeNode<int> root)
+        {
+
+            if (root == null)
+            {
+                return false;
+            }
+
+            var queue = new Queue<BinaryTreeNode<int>>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var queueCount = queue.Count;
+                while (queueCount-- > 0)
+                {
+                    var currentNode = queue.Dequeue();
+
+                    if (currentNode.Left != null || currentNode.Right != null)
+                    {
+                        var left = currentNode.Left == null ? 0 : currentNode.Left.Value;
+                        var right = currentNode.Right == null ? 0 : currentNode.Right.Value;
+
+                        if (currentNode.Value != left + right)
+                        {
+                            Console.WriteLine("given tree is not sum tree");
+                            return false;
+                        }
+                    }
+                    if (currentNode.Left != null)
+                    {
+                        queue.Enqueue(currentNode.Left);
+                    }
+                    if(currentNode.Right != null)
+                    {
+                        queue.Enqueue(currentNode.Right);
+                    }
+                }
+            }
+
+            Console.WriteLine("given tree is sum tree");
+            return true;
+        }
+
+        public static int CalculateSumTree(BinaryTreeNode<int> root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int left = CalculateSumTree(root.Left);
+
+            int right = CalculateSumTree(root.Right);
+
+            if (left != 0 || right != 0)
+            {
+                root.Value = left + right;
+            }
+
+            return root.Value;
+        }
+    }
+}
---
+++ BinaryTreeAndGrahp/BinaryTree.23.CheckIfGivenTreeIsASubTree/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree._23.CheckIfGivenTreeIsASubTree
+{
+    /// <summary>
+    /// https://www.techiedelight.com/determine-given-binary-tree-is-subtree-of-another-binary-tree-not/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binayTree = new BinaryTree<int>(1);
+
+            // level 2
+            binayTree.Root.Left = new BinaryTreeNode<int>(2);
+            binayTree.Root.Right = new BinaryTreeNode<int>(3);
+
+            // level 3
+            binayTree.Root.Left.Left = new BinaryTreeNode<int>(4);
+            binayTree.Root.Left.Right = new BinaryTreeNode<int>(5);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(6);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(7);
+            // level 4
+            binayTree.Root.Right.Left.Left = new BinaryTreeNode<int>(8);
+            binayTree.Root.Right.Left.Right = new BinaryTreeNode<int>(9);
+
+            binayTree.LevelOrderTraversal();
+
+            var checkTree = new BinaryTree<int>(1);
+
+            // level 2
+            checkTree.Root.Left = new BinaryTreeNode<int>(5);
+            checkTree.Root.Left = new BinaryTreeNode<int>(5);
+            checkTree.Root.Right = new BinaryTreeNode<int>(7);
+
+            IsSubTree(binayTree.Root, binayTree.Root.Right);
+            IsSubTree(binayTree.Root.Right, binayTree.Root);
+            IsSubTree(binayTree.Root.Right, checkTree.Root);
+            Console.ReadLine();
+        }
+
+        public static void IsSubTree(BinaryTreeNode<int> firstTree, BinaryTreeNode<int> givenTree)
+        {
+            var firstTreeOrder = new List<int>();
+            InOrderTraversal(firstTree, firstTreeOrder);
+            Console.WriteLine();
+            var secondTreeOrder = new List<int>();
+            InOrderTraversal(givenTree, secondTreeOrder);
+
+            var longerList = firstTreeOrder.Count > secondTreeOrder.Count ? firstTreeOrder : secondTreeOrder;
+            var smallerList = longerList == firstTreeOrder ? secondTreeOrder : firstTreeOrder;
+
+            var indexOfFirst = longerList.IndexOf(smallerList[0]);
+            if (indexOfFirst == -1)
+            {
+                Console.WriteLine("They are not a subtree of each other");
+                return;
+            }
+
+            var firstString = string.Join("", longerList);
+
+            var secondString = string.Join("", smallerList);
+
+            if(firstString.IndexOf(secondString) == -1)
+            {
+                Console.WriteLine("They are not a subtree of each other");
+                return;
+            }
+
+            if(firstTreeOrder.Count > secondTreeOrder.Count)
+            {
+                Console.WriteLine("Second tree is sub tree of first tree");
+            }
+            else
+            {
+                Console.WriteLine("First tree is sub tree of second tree");
+            }
+        }
+
+        public static void InOrderTraversal(BinaryTreeNode<int> root, List<int> results)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            InOrderTraversal(root.Left, results);
+            results.Add(root.Value);
+            InOrderTraversal(root.Right, results);
+        }
+
+    }
+}
