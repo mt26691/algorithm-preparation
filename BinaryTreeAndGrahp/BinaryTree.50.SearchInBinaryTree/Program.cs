569196854442c2e136208f23e10b942754873d6c
mt26691 <tung.nguyen@scopicsoftware.com>
2019-07-28 09:59:06 +0700

    create bst from given keys

---
+++ BinaryTreeAndGrahp/BinaryTree.50.SearchInBinaryTree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace BinaryTree._50.SearchInBinaryTree
+{
+
+    /// <summary>
+    /// https://www.techiedelight.com/search-given-key-in-bst/?source=post_page---------------------------
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binayTree = new BinarySearchTree<int>(15);
+
+            // level 2
+            binayTree.Root.Left = new BinaryTreeNode<int>(10);
+            binayTree.Root.Right = new BinaryTreeNode<int>(20);
+
+            // level 3
+            binayTree.Root.Left.Left = new BinaryTreeNode<int>(8);
+            binayTree.Root.Left.Right = new BinaryTreeNode<int>(12);
+            binayTree.Root.Right.Left = new BinaryTreeNode<int>(18);
+            binayTree.Root.Right.Right = new BinaryTreeNode<int>(25);
+
+            // level 4
+            binayTree.Root.Right.Left.Left = new BinaryTreeNode<int>(18);
+            binayTree.Root.Right.Left.Right = new BinaryTreeNode<int>(19);
+            binayTree.Root.Right.Right.Right = new BinaryTreeNode<int>(30);
+
+            binayTree.LevelOrderTraversal();
+            var data = FindData(binayTree.Root, 31);
+            if(data != null)
+            {
+                Console.WriteLine($"Result = {data.Value}");
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+
+        public static BinaryTreeNode<int> FindData(BinaryTreeNode<int> root, int value)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            var currentNode = root;
+            while (true)
+            {
+                if (currentNode.Value == value)
+                {
+                    return currentNode;
+                }
+                if (currentNode.Value < value && currentNode.Right != null)
+                {
+                    currentNode = currentNode.Right;
+                }
+                else if (currentNode.Value > value && currentNode.Left != null)
+                {
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+        }
+
+    }
+}
---
+++ BinaryTreeAndGrahp/BinaryTree.51.CreateBinaryTreeFromGivenTree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace BinaryTree._51.CreateBinaryTreeFromGivenTree
+{
+    /// <summary>
+    /// https://www.techiedelight.com/search-given-key-in-bst/?source=post_page---------------------------
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTreeNode<int> root = null;
+            var keys = new int[] { 15, 10, 20, 8, 12, 18, 25 };
+            keys = keys.OrderBy(t => t).ToArray();
+
+            root = CreateTree(keys, 0, keys.Length - 1, root);
+
+            root.LevelOrderTraversal();
+        }
+
+        public static BinaryTreeNode<int> CreateTree(int[] givenSortedKeys, int low, int high, BinaryTreeNode<int> root)
+        {
+            if (low > high)
+            {
+                return root;
+            }
+
+            int mid = (low + high) / 2;
+
+            root = new BinaryTreeNode<int>(givenSortedKeys[mid]);
+
+            root.Left = CreateTree(givenSortedKeys, low, mid - 1, root.Left);
+
+            root.Right = CreateTree(givenSortedKeys, mid + 1, high, root.Right);
+
+            return root;
+        }
+
+    }
+}
